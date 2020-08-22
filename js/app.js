464d177c8f3175cca020a96c40a5eafbedfb2812
shiney1884 <scott0406@outlook.com>
2020-08-22 16:47:35 +0100

    added divide functionality

--- js/app.js
+++ js/app.js
@@ -2,7 +2,20 @@ const allClearBtn = document.getElementById('all-clear');
 const clearCurrent = document.getElementById('clear')
 const prevNum = document.getElementById('prev')
 const currentNum = document.getElementById('current')
-const number = document.getElementsByClassName('number');
+const point = document.getElementById('.')
+const zero = document.getElementById('0');
+const one = document.getElementById('1');
+const two = document.getElementById('2');
+const three = document.getElementById('3');
+const four = document.getElementById('4');
+const five = document.getElementById('5');
+const six = document.getElementById('6');
+const seven = document.getElementById('7');
+const eight = document.getElementById('8');
+const nine = document.getElementById('9');
+const divideBtn = document.getElementById('divide');
+
+
 
 allClearBtn.addEventListener('click', function() {
     prevNum.innerHTML = '';
@@ -13,8 +26,59 @@ clearCurrent.addEventListener('click', function() {
     currentNum.innerHTML = '';
 });
 
-
-number.addEventListener('click', function() {
-    console.log(number.innerHTML)
+point.addEventListener('click', function() {
+    if(!currentNum.innerHTML.includes('.')) {
+        currentNum.innerHTML += point.innerHTML
+    }
 })
 
+zero.addEventListener('click', function() {
+    currentNum.innerHTML += zero.innerHTML;
+})
+
+one.addEventListener('click', function () {
+    currentNum.innerHTML += one.innerHTML;
+})
+
+two.addEventListener('click', function () {
+    currentNum.innerHTML += two.innerHTML;
+})
+
+three.addEventListener('click', function () {
+    currentNum.innerHTML += three.innerHTML;
+})
+
+four.addEventListener('click', function () {
+    currentNum.innerHTML += four.innerHTML;
+})
+
+five.addEventListener('click', function () {
+    currentNum.innerHTML += five.innerHTML;
+})
+
+six.addEventListener('click', function () {
+    currentNum.innerHTML += six.innerHTML;
+})
+
+seven.addEventListener('click', function () {
+    currentNum.innerHTML += seven.innerHTML;
+})
+
+eight.addEventListener('click', function () {
+    currentNum.innerHTML += eight.innerHTML;
+})
+
+nine.addEventListener('click', function () {
+    currentNum.innerHTML += nine.innerHTML;
+})
+
+divideBtn.addEventListener('click', function () {
+  if (prevNum.innerHTML === '') {
+      prevNum.innerHTML += currentNum.innerHTML
+      currentNum.innerHTML = '';
+  }
+  if (prevNum.innerHTML !== '' && currentNum.innerHTML !== '') {
+        currentNum.innerHTML = +prevNum.innerHTML/+currentNum.innerHTML;
+        prevNum.innerHTML = '';
+  }
+});
